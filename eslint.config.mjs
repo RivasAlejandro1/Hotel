a03c97600525bb923f4565871050723adf4ad938
AlejandroRivas <rivasalejandro232323@gmail.com>
2024-11-05 18:25:27 -0600

    eslint configuration and nodemon

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,17 @@ import pluginJs from "@eslint/js";
 
 
 /** @type {import('eslint').Linter.Config[]} */
+
 export default [
-  {languageOptions: { globals: globals.node }},
+  {
+    languageOptions: { globals: globals.node },
+  },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      "prefer-const": "error"
+    }
+  },
+
+  
 ];
