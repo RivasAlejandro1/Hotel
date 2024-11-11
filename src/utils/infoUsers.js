6593958e0d19929e7bd5bfcd781412cb98a0d958
AlejandroRivas <rivasalejandro232323@gmail.com>
2024-11-11 17:35:58 -0600

    Repair user seeder info

---
+++ src/utils/infoUsers.js
@@ -0,0 +1,74 @@
+const infoUsers = [
+    {
+        name: "Pedro",
+        lastName: "Martinez",
+        birthdate: "1990-05-15",
+        cedula: 1234567890,
+        administrador: true
+    },
+    {
+        name: "Maria",
+        lastName: "Gomez",
+        birthdate: "1985-08-25",
+        cedula: 1234567891,
+        administrador: false
+    },
+    {
+        name: "Carlos",
+        lastName: "Sanchez",
+        birthdate: "1992-03-10",
+        cedula: 1234567892,
+        administrador: true
+    },
+    {
+        name: "Ana",
+        lastName: "Lopez",
+        birthdate: "1988-11-30",
+        cedula: 1234567893,
+        administrador: false
+    },
+    {
+        name: "Luis",
+        lastName: "Perez",
+        birthdate: "1982-06-18",
+        cedula: 1234567894,
+        administrador: true
+    },
+    {
+        name: "Laura",
+        lastName: "Ramirez",
+        birthdate: "1995-01-22",
+        cedula: 1234567895,
+        administrador: false
+    },
+    {
+        name: "Jose",
+        lastName: "Diaz",
+        birthdate: "1979-12-05",
+        cedula: 1234567896,
+        administrador: true
+    },
+    {
+        name: "Clara",
+        lastName: "Hernandez",
+        birthdate: "1993-07-17",
+        cedula: 1234567897,
+        administrador: false
+    },
+    {
+        name: "Raul",
+        lastName: "Vega",
+        birthdate: "1991-09-09",
+        cedula: 1234567898,
+        administrador: true
+    },
+    {
+        name: "Elena",
+        lastName: "Fernandez",
+        birthdate: "1987-04-20",
+        cedula: 1234567899,
+        administrador: false
+    }
+];
+
+export default infoUsers;
