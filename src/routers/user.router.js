3c5e8a8ff767c3830788791a7b4bb63f8d5fedee
AlejandroRivas <rivasalejandro232323@gmail.com>
2024-11-11 17:36:37 -0600

    add middleware to getUserByIDRouter

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,8 +1,17 @@
 import { Router } from 'express';
-import { getAllUserController } from '../controllers/user.controller.js';
+import { getAllUserController, getUserByIdController } from '../controllers/user.controller.js';
 const userRouter = Router();
 
 userRouter.get("/", getAllUserController);
-userRouter.get("/:id", getAllUserController); 
+userRouter.get("/:id", (req, res, next)=>{
+    const validarNumero = /^\d+$/ 
+    
+    if(!validarNumero.test(req.params.id)) {
+        res.status(400).send("El id debe ser un numero");
+    } else{
+        next();
+    }
+}); 
+userRouter.get("/:id", getUserByIdController); 
 
 export default userRouter;
