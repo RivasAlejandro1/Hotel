7739c7273acf1382c8486b85967c206a0d5c9940
AlejandroRivas <rivasalejandro232323@gmail.com>
2024-11-11 17:37:12 -0600

    Repair flow getUserByID

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import { getAllUserService } from "../services/user.service.js";
+import { getAllUserService, getUserByIDService } from "../services/user.service.js";
 
 
 export const getAllUserController =  async (req, res)=>{
@@ -12,14 +12,17 @@ export const getAllUserController =  async (req, res)=>{
     }
 
 }
-
 export const getUserByIdController =  async (req, res)=>{
     try{
-        const allUser =  await getAllUserService();
-        res.status(200).send(allUser);
+        const userFinded =  await getUserByIDService(req.params.id);
+        if(userFinded == null) throw new Error(`Usuario con el id ${req.params.id} no existe.`)
+        res.status(200).send(userFinded);
     }
     catch(error){
-        res.status(404).send(error);
+        res.status(404).send(
+            {
+                error: error.message
+            });
     }
 
 } 
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,15 +3,32 @@ import User from "../entities/user.entity.js";
 import infoUsers from "../utils/infoUsers.js";
 
 const userReposository = AppDataSource.getRepository(User);
+
 export const getAllUserService = async ()=> {
     const allUser = await userReposository.find();
     return allUser;
 };
 
-export const getUserByIDService = async ()=> {
-    const userFinded = await userReposository.findOneBy("id");
+export const getUserByIDService = async (id)=> {
+    const userFinded = await userReposository.findOneBy({id});
     return userFinded;
 };
+
+export const createUserService = async (user)=> {
+    const userFinded = await userReposository.save(user);
+    return userFinded;
+};
+
+export const modifeUserService = async (id, user)=> {
+    const userFinded = await userReposository
+    .update(
+        {id},
+        user
+    );
+    return userFinded;
+};
+
+
 
 export const userSeederService = async ()=> {
 
