7df815b642cae2b0fa7e1f17da2bb488e6c0ba5f
AlejandroRivas <rivasalejandro232323@gmail.com>
2024-11-13 16:21:00 -0600

    add middlewares for userModife

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { createUserController, getAllUserController, getUserByIdController, modifeUserController } from '../controllers/user.controller.js';
-import validateUser from '../validations/createUserValidation.js';
+import {validateUser, validateName, validateBirthdate,  validateCedula} from '../validations/createUserValidation.js';
 const userRouter = Router();
 
 userRouter.get("/", getAllUserController);
@@ -36,5 +36,38 @@ userRouter.post("/", (req, res, next)=>{
 });
 userRouter.post("/", createUserController); 
 
+userRouter.put("/:id", (req, res, next) =>{
+    const { name, lastName, birthdate, cedula} = req.body; 
+    const newBody = {};
+    try{
+        const id = req.params.id;
+        if(id?.length == 0) throw new Error ("Debes enviar un id por params");
+        if(!name && !lastName && !birthdate && !cedula) throw new Error("Debes enviar algun cambio por body con los nombres: name, birthdate, lastName o cedula");
+        
+        if(name) {
+            validateName(name, "nombre");
+            newBody.name = name;
+        };
+        if(birthdate){
+            validateBirthdate(birthdate);
+            newBody.birthdate = birthdate;
+        } 
+        if(lastName){
+            validateName(lastName, "apellido");
+            newBody.lastName = lastName;
+        } 
+        if(cedula) {
+            validateCedula(cedula)
+            newBody.cedula = cedula;
+        }
+        req.body = newBody;
+        next();
+        
+    }
+    catch(error){
+        res.status(400).send(error.message)
+    }
+    
+})
 userRouter.put("/:id", modifeUserController); 
 export default userRouter;
--- src/validations/createUserValidation.js
+++ src/validations/createUserValidation.js
@@ -7,19 +7,19 @@ const itHasANumberValidation = /\d/;
 const cedulaLengthValidation = /^\d{7,8}$/;
 const onlyNumbers = /^\d+$/;
 
-function validateName(name, text){
+export function validateName(name, text){
     if(itHasANumberValidation.test(name)) throw new Error(`El ${text} no puede contener numeros`)
     if(containsASpecialCharaterValidation.test(name)) throw new Error(`El ${text} no puede contener un caracter especial`)
     if(!lengthValidation.test(name)) throw new Error(`El ${text} debe contener entre 5 a 50 letras`)
                 
 }
             
-function validateCedula(cedula){
+export function validateCedula(cedula){
     if(!onlyNumbers.test(cedula)) throw new Error(`La cedula solo debe contener numeros`)
     if(!cedulaLengthValidation.test(cedula)) throw new Error(`La cedula debe contener entre 7 y 8 numeros`)           
 }
 
-function validateBirthdate(infoDate){
+export function validateBirthdate(infoDate){
     if(Number.isNaN(Date.parse(infoDate))) throw new Error(`La fecha ${infoDate} no es un formato de fecha valido`);
     const date = new Date(infoDate);
     const current = new Date();
@@ -30,7 +30,7 @@ function validateBirthdate(infoDate){
 }   
 
 
-export default function validateUser(name, lastName, birthdate, cedula){
+export function validateUser(name, lastName, birthdate, cedula){
     validateName(name, "nombre");
     validateName(lastName, "apellido");
     validateCedula(cedula);
