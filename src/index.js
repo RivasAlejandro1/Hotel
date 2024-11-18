a4154a559f88ff013668ec673712ae3c53cb79d7
AlejandroRivas <rivasalejandro232323@gmail.com>
2024-11-17 23:06:57 -0600

    add reservationSeeder

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import "reflect-metadata";
 const PORT = 3000;
 import { AppDataSource } from "./config/AppDataSource.js";
 import { getAllUserService, userSeederService } from "./services/user.service.js";
-import { roomSeederService } from "./services/room.service.js";
+import { getAllRoomsService, roomSeederService } from "./services/room.service.js";
 import { credentialSeederService } from "./services/credential.service.js";
+import { reservationSeederService } from "./services/reservation.service.js";
 
 AppDataSource.initialize()
     .then(() => {
@@ -15,9 +16,11 @@ AppDataSource.initialize()
     .then(async () =>{
       const users = await getAllUserService();
       if(users.length == 0){
-        credentialSeederService();
-        roomSeederService();
-        //userSeederService();
+        await credentialSeederService();
+        await roomSeederService();
+        const users = await getAllUserService();
+        const rooms = await getAllRoomsService();
+        reservationSeederService(users, rooms);
       }
     })
     .catch((error) => console.log(error))
--- src/services/reservation.service.js
+++ src/services/reservation.service.js
@@ -1,6 +1,6 @@
 import { AppDataSource } from "../config/AppDataSource.js";
 import reservationEntity from "../entities/reservation.entity.js";
- 
+import infoReservations from "../utils/infoReservations.js";
 
 const reservationRepository = AppDataSource.getRepository(reservationEntity);
 
@@ -9,3 +9,18 @@ export const getAllReservationsService =  async () =>{
     const allReservations = await reservationRepository.find();
     return allReservations;
 }
+
+
+export const reservationSeederService = async (users, rooms)=> {
+    let count = 0;
+    await Promise.all(
+        infoReservations.map( async(reservation) => {
+           if(count > 2) count = 0;
+           reservation.user = users[count];
+           reservation.room = rooms[count];
+           count = count +1;
+            await reservationRepository.save(reservation);
+   })) 
+
+   console.log("¡reservation Seeder done!");
+};
