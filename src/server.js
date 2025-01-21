ed94b1f7121f0d0c040ce6c158bf9640a4cb00cf
AlejandroRivas <rivasalejandro232323@gmail.com>
2025-01-20 18:22:49 -0600

    Add cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import router from './routers/index.js';
+import cors from 'cors';
 const server = express();
 
 
 server.use(express.json());
+server.use(cors());
 server.use(router);
 
 export default server;
